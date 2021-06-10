2f7b8e13056f69d0c6347e699ab18fa026163d8a
brentkrum <brentkrum@hotmail.com>
2021-06-10 15:40:52 -0700

    Updates to Minimal HTTP request classes

--- project/thebk-core/src/main/java/com/thebk/utils/http/MinimalHTTPRequest.java
+++ project/thebk-core/src/main/java/com/thebk/utils/http/MinimalHTTPRequest.java
@@ -6,12 +6,10 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.MalformedURLException;
-import java.net.Socket;
-import java.net.URL;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import javax.net.SocketFactory;
 
@@ -20,10 +18,10 @@ public class MinimalHTTPRequest {
 	private MinimalHTTPRequest() {
 	}
 
-	// TODO need to add optional headers to GET
-	// TODO need to add POST
-
 	public static String get(String urlString) {
+		return get(urlString, (Map<String, String>)null);
+	}
+	public static String get(String urlString, final Map<String, String> headers) {
 		final Logger LOG = LoggerFactory.getLogger(MinimalHTTPRequest.class.getName());
 		final URL url;
 		try {
@@ -32,31 +30,85 @@ public class MinimalHTTPRequest {
 			LOG.error("", e);
 			throw new IllegalArgumentException("Could not parse URL '" + urlString + "'", e);
 		}
-		return get(url);
+		return get(url, headers);
 	}
-
 	public static String get(URL url) {
-		if (url.getProtocol() == "https") {
-			return MinimalHTTPSRequest.get(url);
+		return get(url, null);
+	}
+	public static String get(URL url, final Map<String, String> headers) {
+		if (url.getProtocol().equals("https")) {
+			return MinimalHTTPSRequest.get(url, headers);
 		} else {
 			final int port = (url.getPort() == -1) ? 80 : url.getPort();
-			return get(url.getHost(), port, url.getFile());
+			return get(url.getHost(), port, url.getFile(), headers);
 		}
 	}
-
 	public static String get(String host, String file) {
 		return get(host, 80, file);
 	}
-
+	public static String get(String host, String file, final Map<String, String> headers) {
+		return get(host, 80, file, headers);
+	}
 	public static String get(String host, int port, String file) {
+		return get(host, port, file, null);
+	}
+	public static String get(String host, int port, String file, final Map<String, String> headers) {
 		final Logger LOG = LoggerFactory.getLogger(MinimalHTTPRequest.class.getName() + "." + host);
 		final SocketFactory factory = SocketFactory.getDefault();
-		return get(factory, host, port, file, 15000, LOG);
+		return execute(factory, "GET", host, port, file, 15000, LOG, null, headers, null);
 	}
 	static String get(final SocketFactory factory, String host, int port, String file, final Logger LOG) {
-		return get(factory, host, port, file, 15000, LOG);
+		return execute(factory, "GET", host, port, file, 15000, LOG, null, null, null);
 	}
-	static String get(final SocketFactory factory, String host, int port, String file, final int readTimeoutInMS, final Logger LOG) {
+	static String get(final SocketFactory factory, String host, int port, String file, final Map<String, String> headers, final Logger LOG) {
+		return execute(factory, "GET", host, port, file, 15000, LOG, null, headers, null);
+	}
+
+	public static String post(String urlString, String contentType, String postedData) {
+		return post(urlString, contentType, (Map<String, String>)null, postedData);
+	}
+	public static String post(String urlString, String contentType, final Map<String, String> headers, String postedData) {
+		final Logger LOG = LoggerFactory.getLogger(MinimalHTTPRequest.class.getName());
+		final URL url;
+		try {
+			url = new URL(urlString);
+		} catch (MalformedURLException e) {
+			LOG.error("", e);
+			throw new IllegalArgumentException("Could not parse URL '" + urlString + "'", e);
+		}
+		return post(url, contentType, postedData);
+	}
+	public static String post(URL url, String contentType, String postedData) {
+		if (url.getProtocol().equals("https")) {
+			return MinimalHTTPSRequest.post(url, contentType, postedData);
+		} else {
+			final int port = (url.getPort() == -1) ? 80 : url.getPort();
+			return post(url.getHost(), port, url.getFile(), contentType, postedData);
+		}
+	}
+	public static String post(String host, String file, String contentType, String postedData) {
+		return post(host, file, contentType, postedData);
+	}
+	public static String post(String host, String file, String contentType, final Map<String, String> headers, String postedData) {
+		return post(host, 80, file, contentType, headers, postedData);
+	}
+	public static String post(String host, int port, String file, String contentType, String postedData) {
+		return post(host, port, file, contentType, null, postedData);
+	}
+	public static String post(String host, int port, String file, String contentType, final Map<String, String> headers, String postedData) {
+		final Logger LOG = LoggerFactory.getLogger(MinimalHTTPRequest.class.getName() + "." + host);
+		final SocketFactory factory = SocketFactory.getDefault();
+		return execute(factory, "POST", host, port, file, 15000, LOG, contentType, headers, postedData);
+	}
+	static String post(final SocketFactory factory, String host, int port, String file, final Logger LOG, String contentType, String postedData) {
+		return execute(factory, "POST", host, port, file, 15000, LOG, contentType, null, postedData);
+	}
+	static String post(final SocketFactory factory, String host, int port, String file, final Logger LOG, String contentType, final Map<String, String> headers, String postedData) {
+		return execute(factory, "POST", host, port, file, 15000, LOG, contentType, headers, postedData);
+	}
+
+
+	static String execute(final SocketFactory factory, String method, String host, int port, String file, final int readTimeoutInMS, final Logger LOG, final String contentType, final Map<String, String> headers, final String postedData) {
 		final StringBuilder responseBuilder = new StringBuilder(8096);
 		final InetAddress addr;
 		try {
@@ -69,26 +121,42 @@ public class MinimalHTTPRequest {
 			if (LOG.isDebugEnabled()) {
 				LOG.debug("Connecting to {} on port {}", host, port);
 			}
-			final Socket socket = factory.createSocket(addr, port);
+			final Socket socket = factory.createSocket();
 			socket.setSoTimeout(readTimeoutInMS);
+
+			// Send request to the web server
+			socket.connect(new InetSocketAddress(addr, port), readTimeoutInMS);
 			try {
 				if (LOG.isDebugEnabled()) {
 					LOG.debug("Sending verb string and headers");
 				}
-				// Send request to the web server
 				final OutputStream out = socket.getOutputStream();
 				try {
+					final byte[] postedDataBytes = (postedData == null) ? null : postedData.getBytes(StandardCharsets.UTF_8);
+
 					StringBuilder request = new StringBuilder();
-					request.append("GET ").append(file).append(" HTTP/1.1\r\n");
+					request.append(method).append(" ").append(file).append(" HTTP/1.1\r\n");
 					request.append("Host: ").append(host);
 					if (port != 80) {
 						request.append(':').append(port);
 					}
 					request.append("\r\n");
+					if (headers != null) {
+						for(Map.Entry<String, String> e : headers.entrySet()) {
+							request.append(e.getKey()).append(": ").append(e.getValue()).append("\r\n");
+						}
+					}
+					if (postedData != null) {
+						request.append("Content-Type: ").append(contentType).append("\r\n");
+						request.append("Content-Length: ").append(postedDataBytes.length).append("\r\n");
+					}
 					request.append("Connection: close\r\n");
 					request.append("\r\n");
 					final byte[] requestBytes = request.toString().getBytes(Charset.forName("ASCII"));
 					out.write(requestBytes);
+					if (postedDataBytes != null) {
+						out.write(postedDataBytes);
+					}
 					out.flush();
 
 					if (LOG.isDebugEnabled()) {
@@ -108,11 +176,11 @@ public class MinimalHTTPRequest {
 						// Now find and skip headers
 						for(int i=0; i<responseBuilder.length()-1; i++) {
 							if (responseBuilder.charAt(i) == '\r'
-									&& responseBuilder.charAt(i+1) == '\n'
-									&& i+3 < responseBuilder.length()
-									&& responseBuilder.charAt(i+2) == '\r'
-									&& responseBuilder.charAt(i+3) == '\n'
-									) {
+								&& responseBuilder.charAt(i+1) == '\n'
+								&& i+3 < responseBuilder.length()
+								&& responseBuilder.charAt(i+2) == '\r'
+								&& responseBuilder.charAt(i+3) == '\n'
+							) {
 								responseBuilder.delete(0, i+4);
 							}
 						}
@@ -134,4 +202,5 @@ public class MinimalHTTPRequest {
 
 		return responseBuilder.toString();
 	}
+
 }
--- project/thebk-core/src/main/java/com/thebk/utils/http/MinimalHTTPSRequest.java
+++ project/thebk-core/src/main/java/com/thebk/utils/http/MinimalHTTPSRequest.java
@@ -1,6 +1,7 @@
 package com.thebk.utils.http;
 
 import java.net.URL;
+import java.util.Map;
 
 import javax.net.SocketFactory;
 import javax.net.ssl.SSLSocketFactory;
@@ -18,21 +19,65 @@ public class MinimalHTTPSRequest {
 	}
 
 	public static String get(URL url) {
+		return get(url, null);
+	}
+	public static String get(URL url, final Map<String, String> headers) {
 		if (url.getProtocol() == "http") {
 			return MinimalHTTPRequest.get(url);
 		} else {
 			final int port = (url.getPort() == -1) ? 443 : url.getPort();
-			return get(url.getHost(), port, url.getFile());
+			return get(url.getHost(), port, url.getFile(), headers);
 		}
 	}
 
 	public static String get(String host, String file) {
-		return get(host, 443, file);
+		return get(host, 443, file, null);
+	}
+	public static String get(String host, String file, final Map<String, String> headers) {
+		return get(host, 443, file, headers);
 	}
 
 	public static String get(String host, int port, String file) {
+		return get(host, port, file, null);
+	}
+
+	public static String get(String host, int port, String file, final Map<String, String> headers) {
 		final Logger LOG = LoggerFactory.getLogger(MinimalHTTPSRequest.class.getName() + "." + host);
 		final SocketFactory factory = SSLSocketFactory.getDefault();
-		return MinimalHTTPRequest.get(factory, host, port, file, LOG);
+		return MinimalHTTPRequest.get(factory, host, port, file, headers, LOG);
+	}
+
+	public static String post(String urlString, String contentType, String postedData) {
+		return post(urlString, contentType, (Map<String, String>)null, postedData);
+	}
+	public static String post(String urlString, String contentType, final Map<String, String> headers, String postedData) {
+		return MinimalHTTPRequest.post(urlString, contentType, postedData);
+	}
+	public static String post(URL url, String contentType, String postedData) {
+		return post(url, contentType, null, postedData);
+	}
+	public static String post(URL url, String contentType, final Map<String, String> headers, String postedData) {
+		if (url.getProtocol() == "http") {
+			return MinimalHTTPRequest.post(url, contentType, postedData);
+		} else {
+			final int port = (url.getPort() == -1) ? 443 : url.getPort();
+			return post(url.getHost(), port, url.getFile(), contentType, postedData);
+		}
+	}
+	public static String post(String host, String file, String contentType, String postedData) {
+		return post(host, file, contentType, null, postedData);
+	}
+	public static String post(String host, String file, String contentType, final Map<String, String> headers, String postedData) {
+		return post(host, 443, file, contentType, headers, postedData);
+	}
+	public static String post(String host, int port, String file, String contentType, String postedData) {
+		final Logger LOG = LoggerFactory.getLogger(MinimalHTTPSRequest.class.getName() + "." + host);
+		final SocketFactory factory = SSLSocketFactory.getDefault();
+		return MinimalHTTPRequest.post(factory, host, port, file, LOG, contentType, postedData);
+	}
+	public static String post(String host, int port, String file, String contentType, final Map<String, String> headers, String postedData) {
+		final Logger LOG = LoggerFactory.getLogger(MinimalHTTPSRequest.class.getName() + "." + host);
+		final SocketFactory factory = SSLSocketFactory.getDefault();
+		return MinimalHTTPRequest.post(factory, host, port, file, LOG, contentType, headers, postedData);
 	}
 }
